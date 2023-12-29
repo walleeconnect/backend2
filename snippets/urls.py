7ad6e2d50f2e2cb857c1b245f820ad4c6fe78cdc
walleeconnect <walleeconnect@gmail.com>
2023-12-29 14:33:20 +0530

    m

---
+++ snippets/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from snippets import views
+from rest_framework.urlpatterns import format_suffix_patterns
+
+urlpatterns = [
+    path('snippets/', views.snippet_list),
+    path('snippets/<int:pk>/', views.snippet_detail),
+    #path('users/', views.UserList.as_view()),
+    #path('users/<int:pk>/', views.UserDetail.as_view()),
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
